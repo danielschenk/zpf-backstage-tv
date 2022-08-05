501e1711e7c928bd1fd535562ef0fd821ba91fd4
Daniël Schenk <danielschenk@gmail.com>
2022-08-05 22:22:36 +0200

    Work on dynamic buttons

--- static/index.js
+++ static/index.js
@@ -1,6 +1,52 @@
 function setDressingRoom(actKey, roomNumber) {
+    hideButtons();
     fetch("dressing_rooms/" + actKey, {
         "method": "PUT",
         "body": roomNumber
-    })
+    }).then(updateAllDressingRoomButtons);
+}
+
+function updateAllDressingRoomButtons() {
+    hideButtons();
+    document.querySelectorAll('button[class="dressing-room"]')
+        .forEach(value => {
+            value.classList.remove("selected");
+            value.disabled = false;
+            console.log(value);
+        });
+
+    fetch("dressing_rooms")
+        .then(response => response.json())
+        .then(data => {
+            for(key in data) {
+                let room = data[key];
+                let button = document.getElementById(key + "-" + room);
+                button.classList.add("selected");
+                button.disabled = true;
+            }
+
+            showButtons();
+        });
+}
+
+function hideButtons() {
+    document.querySelectorAll('div[class="dressing-room-buttons"]')
+        .forEach(value => {
+            value.style.display = "none";
+        });
+    document.querySelectorAll('div[class="dressing-room-loader"]')
+        .forEach(value => {
+            value.style.display = "block";
+        });
+}
+
+function showButtons() {
+    window.top.document.querySelectorAll('div[class="dressing-room-buttons"]')
+        .forEach(value => {
+            value.style.display = "block";
+        });
+    window.top.document.querySelectorAll('div[class="dressing-room-loader"]')
+        .forEach(value => {
+            value.style.display = "none";
+        });
 }
