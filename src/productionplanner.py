44f67af6768d587d6416b81e420891bca3e7cdd7
Daniël Schenk <danielschenk@gmail.com>
2025-07-25 22:18:48 +0200

    fix: depreaction warning from re.sub with positional count

--- src/productionplanner.py
+++ src/productionplanner.py
@@ -5,4 +5,6 @@ import re
 
 def remove_friends_night_tag(act_name: str):
     """Removes any tag indicating a "Vriendenavond" (Friends Night) production from an act name"""
-    return re.sub(r" *[@[\(] *vrienden *(avond)*[]\)]*$", "", act_name, 1, flags=re.IGNORECASE)
+    return re.sub(
+        r" *[@[\(] *vrienden *(avond)*[]\)]*$", "", act_name, count=1, flags=re.IGNORECASE
+    )
